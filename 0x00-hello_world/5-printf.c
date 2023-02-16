c9949e796dd4a40f038887e194fdc0fb378ff9c7
Young Nyamkondiwa <youngnyamkondiwa@gmail.com>
2023-02-16 09:07:37 -0800

    C Hello World task 5 printf commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: displays 0 for success
+ */
+
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
