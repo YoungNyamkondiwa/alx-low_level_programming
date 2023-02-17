2304a98f3bd6b925e4557776aed672d029bd26c2
Young Nyamkondiwa <youngnyamkondiwa@gmail.com>
2023-02-17 10:33:13 -0800

    Task 3 hot fix

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
 /**
- * main - Entry point
+ * main - print alphabet in lower and uppercase
  *
- * Return: displays 0 for success
+ *
+ * Return: (0) Success
  */
-
 int main(void)
 {
-	char ch;
 
-	for (ch = 'a'; ch <= 'z'; ch++
-	putchar(ch);
-
-	for (ch = 'A'; ch <= 'Z'; ch++
-	putchar(ch);
-
-	putchar('\n');
-
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+char(ch);
+	}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+		putchar('\n');
 	return (0);
 }
+
