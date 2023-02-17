b9908675e6f48073528a5e9ca62efa3dc02f2db7
Young Nyamkondiwa <youngnyamkondiwa@gmail.com>
2023-02-17 08:03:06 -0800

    Variables_If_Else_While task 3 commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: displays 0 for success
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++
+	putchar(low)
+
+	for (low = 'A'; low <= 'Z'; low++
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+}
