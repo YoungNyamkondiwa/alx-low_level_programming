ee9d2d8b7b553797506f7e6573c34019a5cc4aed
Young Nyamkondiwa <youngnyamkondiwa@gmail.com>
2023-02-21 09:43:19 -0800

    Functions_Nested_Loops 6 task commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _abs - prints the absolute value of an int.
+*
+* @n: input number as an integer.
+*
+* Return: absolute value
+*/
+
+
+int _abs(int n)
+	{
+	if (n >= 0)
+	{
+	return (n);
+	}
+	else
+	{
+	return (n * -1);
+	}
+}
+
