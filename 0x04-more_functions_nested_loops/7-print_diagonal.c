9a20ce1dd1b6415399ed67831b71a6f653868975
Young Nyamkondiwa <youngnyamkondiwa@gmail.com>
2023-02-23 10:54:30 -0800

    More_Functions_Nested_Loops task 7 commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* print_diagonal - draws a diagonal line on stdout.
+*
+* @n: takes in an int
+*/
+
+void print_diagonal(int n)
+
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			if (i == j)
+			{
+				_putchar('\\');
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+	}
+}
