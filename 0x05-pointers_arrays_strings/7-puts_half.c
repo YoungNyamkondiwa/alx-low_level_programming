c75210fb4d1b18c3b5420683d3942bdfd736175f
Young Nyamkondiwa <youngnyamkondiwa@gmail.com>
2023-02-27 14:29:15 -0800

    Pointers_Arrays_Strings task 7 commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ *puts_half - prints half of string.
+ *@str: user input to be evaluated.
+ *Return: half of string
+ */
+
+void puts_half(char *str)
+{
+	int len = 0;
+	char *y = str;
+	int n;
+
+	while (*y != '\0')
+	{
+		y++;
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+	}
+	else
+	{
+		n = (len + 1) / 2;
+	}
+	for ( ; n < len ; n++)
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
+}
+
