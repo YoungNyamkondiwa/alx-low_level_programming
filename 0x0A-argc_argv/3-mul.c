fe6b7e0f928331cdb3a8217c382b7362cbe916cb
Young Nyamkondiwa <youngnyamkondiwa@gmail.com>
2023-03-10 11:12:41 -0800

    0x0A_Argc_Argv task 3 commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,25 +1,22 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * main - multiply two numbers
- * @argc: arguement count
- * @argv: array of pointers to arguement strings
- * Return: result of multiplication or 1
- **/
-
+ * main - prints the multiplication of two integers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if true, 1 if false
+ */
 int main(int argc, char *argv[])
 {
-	int a, b, result;
-
-	if (argc <= 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-/* atoi convert a string to an int */
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-	result = a * b;
-	printf("%d\n", result);
-	return (0);
+int a, b;
+if (argc == 3)
+{
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+printf("%d\n", a *b);
+return (0);
+}
+printf("Error\n");
+return (1);
 }
